88c24932339703af24938268484ca8d55b113703
notfound405 <43387518+185264646@users.noreply.github.com>
2026-09-06 03:53:54 +0800

    ch5: implement sys_set_priority

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -251,10 +251,16 @@ pub fn sys_spawn(path: *const u8) -> isize {
 }
 
 // YOUR JOB: Set task priority.
-pub fn sys_set_priority(_prio: isize) -> isize {
+pub fn sys_set_priority(prio: isize) -> isize {
     trace!(
-        "kernel:pid[{}] sys_set_priority NOT IMPLEMENTED",
+        "kernel:pid[{}] sys_set_priority",
         current_task().unwrap().pid.0
     );
-    -1
+    if prio < 2 {
+        return -1;
+    }
+
+    current_task().unwrap().inner_exclusive_access().priority = prio as u32;
+
+    0
 }
